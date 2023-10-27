4be18b2ea57277167c4151973c2a951893835fa6
Robert Kaussow <mail@thegeeklab.de>
2023-10-27 09:58:52 +0200

    revert to spaces as tabs are not allowed

--- engine/const.go
+++ engine/const.go
@@ -9,45 +9,46 @@ var (
 	CloudInitUserDataUbuntuDefault = `
 #cloud-config
 
-apt_reboot_if_required: false
+package_reboot_if_required: false
 package_update: false
 package_upgrade: false
 
 groups:
-	- docker
+  - docker
 
 system_info:
-	default_user:
-		groups: [ docker ]
+  default_user:
+    groups: [ docker ]
 
 apt:
-	sources:
-		docker.list:
-			source: deb https://download.docker.com/linux/ubuntu $RELEASE stable
-			keyid: 0EBFCD88
+  sources:
+    docker.list:
+      source: deb https://download.docker.com/linux/ubuntu $RELEASE stable
+      keyid: 0EBFCD88
 
 packages:
-	- docker-ce
-	- docker-compose-plugin
+  - docker-ce
+  - docker-compose-plugin
 
 write_files:
 - path: /root/docker-compose.yml
-	content: |
-		# docker-compose.yml
-		version: '3'
-		services:
-			woodpecker-agent:
-				image: {{ .Image }}
-				restart: always
-				volumes:
-					- /var/run/docker.sock:/var/run/docker.sock
-				environment:
-					{{- range $key, $value := .Environment }}
-					- {{ $key }}={{ $value }}
-					{{- end }}
+  content: |
+    # docker-compose.yml
+    version: '3'
+    services:
+      woodpecker-agent:
+        image: {{ .Image }}
+        restart: always
+        volumes:
+          - /var/run/docker.sock:/var/run/docker.sock
+        environment:
+
+          {{- range $key, $value := .Environment }}
+          - {{ $key }}={{ $value }}
+          {{- end }}
 
 runcmd:
-	- sh -xc "cd /root; docker compose up -d"
+  - sh -xc "cd /root; docker compose up -d"
 
 final_message: "The system is finally up, after $UPTIME seconds"
 `
