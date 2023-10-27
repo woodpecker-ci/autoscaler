99e719f232367c7b1c587839e71bf82817fa56ff
Robert Kaussow <mail@thegeeklab.de>
2023-10-27 09:50:05 +0200

    Fix indentation in cloudconfig multiline string

--- engine/const.go
+++ engine/const.go
@@ -7,48 +7,48 @@ var (
 	LabelPool                      = fmt.Sprintf("%spool", LabelPrefix)
 	LabelImage                     = fmt.Sprintf("%simage", LabelPrefix)
 	CloudInitUserDataUbuntuDefault = `
-	#cloud-config
+#cloud-config
 
-	apt_reboot_if_required: false
-	package_update: false
-	package_upgrade: false
+apt_reboot_if_required: false
+package_update: false
+package_upgrade: false
 
-	groups:
-		- docker
+groups:
+	- docker
 
-	system_info:
-		default_user:
-			groups: [ docker ]
+system_info:
+	default_user:
+		groups: [ docker ]
 
-	apt:
-		sources:
-			docker.list:
-				source: deb https://download.docker.com/linux/ubuntu $RELEASE stable
-				keyid: 0EBFCD88
+apt:
+	sources:
+		docker.list:
+			source: deb https://download.docker.com/linux/ubuntu $RELEASE stable
+			keyid: 0EBFCD88
 
-	packages:
-		- docker-ce
-		- docker-compose-plugin
+packages:
+	- docker-ce
+	- docker-compose-plugin
 
-	write_files:
-	- path: /root/docker-compose.yml
-		content: |
-			# docker-compose.yml
-			version: '3'
-			services:
-				woodpecker-agent:
-					image: {{ .Image }}
-					restart: always
-					volumes:
-						- /var/run/docker.sock:/var/run/docker.sock
-					environment:
-						{{- range $key, $value := .Environment }}
-						- {{ $key }}={{ $value }}
-						{{- end }}
+write_files:
+- path: /root/docker-compose.yml
+	content: |
+		# docker-compose.yml
+		version: '3'
+		services:
+			woodpecker-agent:
+				image: {{ .Image }}
+				restart: always
+				volumes:
+					- /var/run/docker.sock:/var/run/docker.sock
+				environment:
+					{{- range $key, $value := .Environment }}
+					- {{ $key }}={{ $value }}
+					{{- end }}
 
-	runcmd:
-		- sh -xc "cd /root; docker compose up -d"
+runcmd:
+	- sh -xc "cd /root; docker compose up -d"
 
-	final_message: "The system is finally up, after $UPTIME seconds"
-	`
+final_message: "The system is finally up, after $UPTIME seconds"
+`
 )
