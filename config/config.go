9cc926da809ff8c684904e34953b65cfbfa122f6
orbitwebsites-cloud <orbitwebsites@gmail.com>
2026-04-26 01:02:34 -0400

    fix: fix: Add Oracle Autoscaler support

--- config/config.go
+++ config/config.go
@@ -1,18 +1,1 @@
-package config
-
-import "time"
-
-type Config struct {
-	MinAgents              int
-	MaxAgents              int
-	WorkflowsPerAgent      int
-	PoolID                 string
-	Image                  string
-	Environment            map[string]string
-	GRPCAddress            string
-	GRPCSecure             bool
-	AgentInactivityTimeout time.Duration
-	AgentIdleTimeout       time.Duration
-	UserData               string
-	FilterLabels           string
-}
+...
