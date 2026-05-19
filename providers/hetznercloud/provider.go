0fc24d5cc4683fcf49d4aee02ffe4eb6af88b5d4
6543 <6543@obermui.de>
2026-05-19 16:23:00 +0200

    Linode re-enable (#606)

--- providers/hetznercloud/provider.go
+++ providers/hetznercloud/provider.go
@@ -94,7 +94,7 @@ func New(_ context.Context, c *cli.Command, config *config.Config) (types.Provid
 }
 
 func (p *Provider) DeployAgent(ctx context.Context, agent *woodpecker.Agent) error {
-	userData, err := cloudinit.RenderUserDataTemplate(p.config, agent, p.userDataTemplate)
+	userData, err := cloudinit.RenderUserDataTemplate(p.config, agent, p.userDataTemplate, cloudinit.RenderOption{})
 	if err != nil {
 		return fmt.Errorf("%s: cloudinit.RenderUserDataTemplate: %w", p.name, err)
 	}
