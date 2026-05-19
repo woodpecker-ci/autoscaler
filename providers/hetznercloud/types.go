38853bc63530e4297a5b9a2f7a24ed1aaa9e01ce
6543 <6543@obermui.de>
2026-05-19 20:59:26 +0200

    merge files

--- providers/hetznercloud/types.go
+++ providers/hetznercloud/types.go
@@ -1,6 +1,20 @@
 package hetznercloud
 
-import "github.com/hetznercloud/hcloud-go/v2/hcloud"
+import (
+	"errors"
+
+	"github.com/hetznercloud/hcloud-go/v2/hcloud"
+)
+
+var (
+	ErrIllegalLabelPrefix   = errors.New("illegal label prefix")
+	ErrImageNotFound        = errors.New("image not found")
+	ErrSSHKeyNotFound       = errors.New("SSH key not found")
+	ErrNetworkNotFound      = errors.New("network not found")
+	ErrFirewallNotFound     = errors.New("firewall not found")
+	ErrServerTypeNotFound   = errors.New("server type not found")
+	ErrLocationNotSupported = errors.New("location not available for server type")
+)
 
 type deployCandidate struct {
 	location   *hcloud.Location
