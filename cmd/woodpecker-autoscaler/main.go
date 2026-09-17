3cdde3202c857f8f2cf1beeaa01c7df0e7454c01
6543 <6543@obermui.de>
2026-09-17 13:15:33 +0200

    Remove Equinix Metal because sunset (#739)

--- cmd/woodpecker-autoscaler/main.go
+++ cmd/woodpecker-autoscaler/main.go
@@ -17,7 +17,6 @@ import (
 	"go.woodpecker-ci.org/autoscaler/engine/types"
 	"go.woodpecker-ci.org/autoscaler/providers/aws"
 	"go.woodpecker-ci.org/autoscaler/providers/digitalocean"
-	"go.woodpecker-ci.org/autoscaler/providers/equinixmetal"
 	"go.woodpecker-ci.org/autoscaler/providers/hetznercloud"
 	"go.woodpecker-ci.org/autoscaler/providers/linode"
 	"go.woodpecker-ci.org/autoscaler/providers/openstack"
@@ -35,8 +34,6 @@ func setupProvider(ctx context.Context, cmd *cli.Command, config *config.Config)
 		return digitalocean.New(ctx, cmd, config)
 	case "hetznercloud":
 		return hetznercloud.New(ctx, cmd, config)
-	case "equinixmetal":
-		return equinixmetal.New(ctx, cmd, config)
 	case "linode":
 		return linode.New(ctx, cmd, config)
 	case "vultr":
@@ -173,7 +170,6 @@ func main() {
 	}
 
 	app.Flags = append(app.Flags, hetznercloud.ProviderFlags...)
-	app.Flags = append(app.Flags, equinixmetal.ProviderFlags...)
 	app.Flags = append(app.Flags, scaleway.ProviderFlags...)
 	app.Flags = append(app.Flags, linode.ProviderFlags...)
 	app.Flags = append(app.Flags, aws.ProviderFlags...)
